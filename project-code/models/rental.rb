5eab3487beea0b680c4a155ca0cb60ecd1e6e3c9
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-27 09:51:03 +0000

    rental object getters and setters

--- project-code/models/rental.rb
+++ project-code/models/rental.rb
@@ -15,4 +15,46 @@ attr_reader :id, :rental_date, :rental_due_date, :return_code, :theatre
     @theatre = options['theatre']
   end
 
+# object getters and setters
+
+  def get_customer_id
+    return @customer_id
+  end
+  def set_customer_id(customer_id)
+    @customer_id = customer_id
+  end
+
+  def get_stock_id
+    return @stock_id
+  end
+  def set_stock_id(stock_id)
+    @stock_id = stock_id
+  end
+
+  def get_transaction_id
+    return @transaction_id
+  end
+  def set_transaction_id(transaction_id)
+    @transaction_id = transaction_id
+  end
+
+   def get_theatre
+     return @theatre
+   end
+   def set_theatre(is_it_theatre)
+     @theatre=is_it_theatre
+   end
+   def set_rental_date(start_date)
+     @rental_date = start_date
+   end
+   def set_rental_due_date(end_date)
+     @rental_due_date = end_date
+   end
+
+   def set_return_code(return_code)
+     @return_code=return_code
+   end
+
+   # end object getters and setters
+
 end # end class
