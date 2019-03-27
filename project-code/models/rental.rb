03036b1452775cf7ace6df0b26cda75ebe3c83d1
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-27 11:07:30 +0000

    Rentals save and delete done

--- project-code/models/rental.rb
+++ project-code/models/rental.rb
@@ -2,18 +2,51 @@ require_relative('../db/sql_runner.rb')
 
 class Rental
 
-attr_reader :id, :rental_date, :rental_due_date, :return_code, :theatre
+attr_reader :id, :rental_date, :return_due_date, :return_code, :theatre
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @customer_id = options['customer_id']
-    @stock_id = options['stock_id']
+    @customer_id = options['customer_id'].to_i
+    @stock_item_id = options['stock_item_id'].to_i
     @rental_date = options['rental_date']
-    @rental_due_date = options['rental_due_date']
-    @return_code = options['return_code']
-    @transaction_id = options['transaction_id']
+    @return_due_date = options['return_due_date']
+    @return_code = options['return_code'].to_i if options['return_code']
+    @transaction_id = options['transaction_id'].to_i if options['transaction_id']
     @theatre = options['theatre']
   end
+
+# database CRUD functions
+
+def save
+  sql = 'INSERT INTO rentals (customer_id, stock_item_id, rental_date, return_due_date, return_code, transaction_id, theatre)
+  VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id;'
+  values = [@customer_id, @stock_item_id, @rental_date, @return_due_date, @return_code, @transaction_id, @theatre]
+  @id = SqlRunner.run(sql, values).first['id'].to_i
+end
+
+def self.list_all
+  sql = 'SELECT * FROM rentals;'
+  return SqlRunner.run(sql).map { |rental| Rental.new(rental)  }
+end
+
+def self.find_by_id(id)
+  sql = 'SELECT * FROM rentals WHERE id = $1;'
+  values = [id]
+  rental = SqlRunner.run(sql, values).first
+  return Rental.new(rental) if rental != nil
+end
+
+def self.delete_all
+  sql = 'DELETE FROM rentals;'
+  SqlRunner.run(sql)
+end
+
+def delete
+  sql = "DELETE FROM rentals WHERE id = $1"
+  values = [@id]
+  SqlRunner.run( sql, values )
+end
+# end database CRUD functions
 
 # object getters and setters
 
@@ -24,11 +57,11 @@ attr_reader :id, :rental_date, :rental_due_date, :return_code, :theatre
     @customer_id = customer_id
   end
 
-  def get_stock_id
-    return @stock_id
+  def get_stock_item_id
+    return @stock_item_id
   end
-  def set_stock_id(stock_id)
-    @stock_id = stock_id
+  def set_stock_item_id(stock_item_id)
+    @stock_item_id = stock_item_id
   end
 
   def get_transaction_id
@@ -47,8 +80,8 @@ attr_reader :id, :rental_date, :rental_due_date, :return_code, :theatre
    def set_rental_date(start_date)
      @rental_date = start_date
    end
-   def set_rental_due_date(end_date)
-     @rental_due_date = end_date
+   def set_return_due_date(end_date)
+     @return_due_date = end_date
    end
 
    def set_return_code(return_code)
--- project-code/specs/rental_specs.rb
+++ project-code/specs/rental_specs.rb
@@ -1,11 +1,96 @@
 require( 'minitest/autorun' )
 require_relative( '../models/rental.rb' )
-
+require( 'minitest/rg' )
+require_relative( '../models/stock_item.rb' )
+require_relative( '../models/customer.rb' )
+require('date')
+require('pry')
 
 class TestRental < MiniTest::Test
 
   def setup
+    Rental.delete_all
+    Customer.delete_all
+    StockItem.delete_all
 
+    @costume1 = {
+      'name' => 'Baby Yoda',
+      'size' => '1',
+      'measurements' => 'Standard Size',
+      'cleaning_instructions' => 'Delicate Wash at 30, No Spin, No Tumble Dry',
+      'status' => '1',
+      'colour' => 'White',
+      'price' => 10
+    }
+    @baby_yoda = StockItem.new(@costume1)
+    @costume2 = {
+      'name' => 'Ensign Redshirt',
+      'size' => '1',
+      'measurements' => 'Standard Size',
+      'cleaning_instructions' => 'Delicate Wash at 30, No Spin, No Tumble Dry',
+      'status' => '1',
+      'colour' => 'Red',
+      'price' => 5
+    }
+    @redshirt = StockItem.new(@costume2)
+    @baby_yoda.save
+    @redshirt.save
+    @customer1 = {
+      'name' => 'Stanley Treshansky',
+      'phone' => '555-1234',
+      'email' => 'stanley@fakeemail.com',
+      'address' => '1 The Road, Happytown',
+      'account_balance' => 10,
+      'warnings' => 0
+    }
+    @customer2 = {
+      'name' => 'Rolf Treshansky',
+      'phone' => '555-5678',
+      'email' => 'rolf@fakeemail.com',
+      'address' => '2 The Road, Happytown',
+      'account_balance' => 100,
+      'warnings' => 1
+    }
+    @customer3 = {
+      'name' => 'Tommy Treshansky',
+      'phone' => '555-3456',
+      'email' => 'tommy@fakeemail.com',
+      'address' => '3 The Road, Happytown',
+      'account_balance' => 0,
+      'warnings' => 2
+    }
+
+    @stanley  = Customer.new(@customer1)
+    @rolf = Customer.new(@customer2)
+    @tommy = Customer.new(@customer3)
+
+    @stanley.save
+    @rolf.save
+    @tommy.save
+
+    @rental1 = {
+      'customer_id' => @stanley.id,
+      'stock_item_id' => @redshirt.id,
+      'rental_date' => Date.today.strftime('%Y-%m-%d'),
+      'return_due_date' => (Date.today+2).strftime('%Y-%m-%d'),
+      'return_code' => nil,
+      'transaction_id' => nil,
+      'theatre' => false
+    }
+    @stanley_redshirt = Rental.new(@rental1)
   end
+
+  def test_created
+    # assert_equal(67, @stanley_redshirt.get_stock_item_id)
+  end
+
+  def test_save_rental
+    @stanley_redshirt.save
+    # p Rental.list_all
+    @test_rental =  Rental.find_by_id(@stanley_redshirt.id)
+    # binding.pry
+    assert_equal(@redshirt.id, @test_rental.get_stock_item_id)
+  end
+
 
 end # end class
