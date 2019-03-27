65270c6a2a07b03b9b56e50f17f19d8c9991ab06
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-27 08:12:03 +0000

    making rental class

--- project-code/models/rental.rb
+++ project-code/models/rental.rb
@@ -1,11 +1,18 @@
 require_relative('../db/sql_runner.rb')
 
-class Rental 
+class Rental
 
-attr_reader :id
+attr_reader :id, :rental_date, :rental_due_date, :return_code, :theatre
 
   def initialize(options)
-
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id']
+    @stock_id = options['stock_id']
+    @rental_date = options['rental_date']
+    @rental_due_date = options['rental_due_date']
+    @return_code = options['return_code']
+    @transaction_id = options['transaction_id']
+    @theatre = options['theatre']
   end
 
 end # end class
