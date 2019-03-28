f3875d763f5258ff90d1bf2f8854a634322b76fd
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-28 10:31:01 +0000

    add new customer working in front end

--- project-code/controller.rb
+++ project-code/controller.rb
@@ -28,12 +28,29 @@ get '/crms/customers' do
   erb(:customers_menu)
 end
 
+get '/crms/customers/customers_new' do
+  erb(:customers_new)
+end
+
 get '/crms/rentals' do
   erb(:rentals_menu)
 end
 
 get '/crms/rentals/current_rentals_list' do
   erb(:current_rentals_list)
+end
+
+post '/crms/customers/process_new' do
+  @my_customer={
+    'name' => params[:name],
+    'phone' => params[:phone],
+    'email' => params[:email],
+    'address' => params[:address],
+    'account_balance' => params[:account_balance],
+    'warnings' => params[:warnings]
+  }
+  Customer.new(@my_customer).save
+  redirect '/crms/customers'
 end
 
 post '/crms/rentals/return' do
@@ -55,5 +72,5 @@ post '/crms/rentals/process_return' do
 #  binding.pry
   @current_rental.return_rental(@return_code, @my_stock_item, @item_status)
   @msg="You returned "+" #{@my_stock_item.name}"
-  redirect '/crms/rentals'
+  redirect '/crms/rentals/current_rentals_list'
 end
