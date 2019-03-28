c9f5dfe6871ef098725e10f702ae31b4d59ab9e5
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-28 01:53:17 +0000

    No method error at process_returns for nil:nilclass in controller.rb line 54

--- project-code/controller.rb
+++ project-code/controller.rb
@@ -35,3 +35,23 @@ end
 get '/crms/rentals/current_rentals_list' do
   erb(:current_rentals_list)
 end
+
+post '/crms/rentals/return' do
+  @item_name = params[:stock_item_name]
+  @due_date = params[:return_due_date]
+  @stock_item_id = params[:stock_item_id]
+  @rental_id=params[:rental_id]
+  @current_rental = Rental.find_by_id(@rental_id)
+  erb(:return)
+end
+
+post '/crms/rentals/process_return' do
+  @stock_item_id = params[:stock_item_id]
+  @return_code=params[:return_code]
+  @item_status=params[:item_status]
+  @my_stock_item = StockItem.find_by_id(@stock_item_id)
+  @current_rental = Rental.find_by_id(@rental_id)
+  @current_rental.return_rental(@return_code, @my_stock_item, @item_status)
+  @msg="You returned "+" #{@my_stock_item.name}"
+  redirect '/crms/rentals'
+end
